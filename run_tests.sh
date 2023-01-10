616d55c9a96319138978e24ebbba75fa3a041f3c
Guilherme Samuel <guisamuel800@gmail.com>
2023-01-09 23:57:54 -0300

    add actions & test files

---
+++ run_tests.sh
@@ -0,0 +1,82 @@
+#!/bin/bash -e
+
+# RobotPy's test file from https://github.com/robotpy/examples/blob/main/run_tests.sh
+
+cd "$(dirname $0)"
+
+# Keep this list alphabetically sorted
+BASE_TESTS="
+  addressableled
+  arcade-drive
+  arm-simulation
+  commands-v2/armbotoffboard
+  commands-v2/drive-distance-offboard
+  commands-v2/frisbee-bot
+  commands-v2/gyro-drive-commands
+  commands-v2/hatchbot
+  commands-v2/hatchbot-inlined
+  commands-v2/ramsete
+  commands-v2/selectcommand
+  cscore-intermediate-vision
+  cscore-quick-vision
+  elevator-profiled-pid
+  elevator-simulation
+  elevator-trapezoid-profile
+  game-data
+  getting-started
+  gyro
+  magicbot-simple
+  mecanum-drive
+  mecanum-driveXbox
+  mechanism2d
+  motor-control
+  physics/src
+  physics-4wheel/src
+  physics-mecanum/src
+  physics-spi/src
+  shuffleboard
+  tank-drive
+  timed/src
+"
+
+IGNORED_TESTS="
+  commands-v2/romi
+  physics-camsim/src
+  stateful-autonomous
+"
+
+ALL_TESTS="${BASE_TESTS}"
+EVERY_TESTS="${ALL_TESTS} ${IGNORED_TESTS}"
+TESTS="${ALL_TESTS}"
+
+TMPD=$(mktemp -d)
+trap 'rm -rf "$TMPD"' EXIT
+
+# Ensure that when new samples are added, they are added to the list of things
+# to test. Otherwise, exit.
+for i in ${EVERY_TESTS}; do
+  echo ./$i/robot.py
+done | sort > $TMPD/a
+
+find . -name robot.py | sort > $TMPD/b
+
+if ! diff -u $TMPD/a $TMPD/b; then
+
+  if [ -z "$FORCE_ANYWAYS" ]; then
+    echo "ERROR: Not every robot.py file is in the list of tests!"
+    exit 1
+  fi
+fi
+
+for t in ${TESTS}; do
+  pushd $t > /dev/null
+  pwd
+  if ! python3 robot.py test --builtin "${@:2}"; then
+    EC=$?
+    echo "Test in $(pwd) failed"
+    exit 1
+  fi
+  popd > /dev/null
+done
+
+echo "All tests successful!"

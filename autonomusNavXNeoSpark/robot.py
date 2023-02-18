93eedcc80f225f9a8bea7dc97a0163cfc389ac83
JulioVianaDev <JulioVianaDev@gmail.com>
2023-02-18 15:36:01 -0300

    deletando as funções não funcionais

--- autonomusNavXNeoSpark/robot.py
+++ autonomusNavXNeoSpark/robot.py
@@ -5,48 +5,7 @@ import rev
 from navx import AHRS
 import wpilib
 class MyRobot(wpilib.TimedRobot):
-    def get_yaw_angle(self):
-        try:
-            # Create an instance of the AHRS class
-            navx = AHRS.create_spi()
-            # Get the current yaw angle
-            yaw = navx.getAngle()
-            # Print the yaw angle
-            # print("Yaw angle: ", yaw)
-            return yaw
-        except Exception as e:
-            print(e)
-            
-    def get_pitch_angle(self):
-        try:
-            # Create an instance of the AHRS class
-            navx = AHRS.create_spi()
-            # Get the current pitch angle
-            pitch = navx.getPitch()
-            # Print the pitch angle
-            # print("Pitch angle: ", pitch)
-            return pitch
-        except Exception as e:
-            print(e)
-
-    def get_roll_angle(self):
-        try:
-            # Create an instance of the AHRS class
-            navx = AHRS.create_spi()
-            # Get the current roll angle
-            roll = navx.getRoll()
-            # Print the roll angle
-            # print("Roll angle: ", roll)
-            return roll
-        except Exception as e:
-            print(e)
-
-    def get_angles(self):
-        return {
-            'pitch': self.get_pitch_angle(),
-            'roll': self.get_roll_angle(),
-            'yall': self.get_yaw_angle()
-        }
+    
     def robotInit(self):
       """Robot initialization function"""
       self.motor = rev.CANSparkMax(52, rev.CANSparkMax.MotorType.kBrushless)

989a932f552d1de7ca57294620a514b81376740e
Guilherme Samuel <guisamuel800@gmail.com>
2023-02-18 15:02:03 -0300

    Add pose

--- photonvision/robot.py
+++ photonvision/robot.py
@@ -1,14 +1,25 @@
 import wpilib
 import photonvision
-import ntcore
+import wpimath
+import wpimath.controller
+
+# Generic Values
+CAMERA_HEIGHT_METERS = 1
+TARGET_HEIGHT_METERS = 1
+CAMERA_PITCH_RADIANS = 1
+GOAL_RANGE_METERS = 2
+
+kP = 0.1
+kI = 0.2
+kD = 0.3
 
 class PhotonVisionTest(wpilib.TimedRobot):
     def robotInit(self) -> None:
-        #nt = ntcore.NetworkTableInstance.getDefault()
-
-        #self.nt.setServerTeam(7459)
-
         self.camera = photonvision.PhotonCamera('Microsoft_LifeCam_HD-3000')
+        self.joystick = wpilib.Joystick(0)
+        self.controller = wpimath.controller.PIDController(
+            0.1, 0.2, 0.3
+        )
 
     def robotPeriodic(self) -> None:
         self.result = self.camera.getLatestResult()
@@ -17,8 +28,21 @@ class PhotonVisionTest(wpilib.TimedRobot):
             print('it worked!')
         else:
             print('it did not work')
-        #print(self.result.getTargets())
-       
+
+    def teleopInit(self) -> None:
+        # Calculating Distance to Target
+        if self.joystick.getRawButton(0):
+            result = self.camera.getLatestResult()
+
+            if result.hasTargets():
+                r = photonvision.PhotonUtils.calculateDistanceToTargetMeters(
+                    CAMERA_HEIGHT_METERS,
+                    TARGET_HEIGHT_METERS,
+                    CAMERA_PITCH_RADIANS,
+                    wpimath.units.degreesToRadians(result.getBestTarget().getPitch())
+                )
+            
+            forward = self.controller.calculate(r, GOAL_RANGE_METERS)
 
 if __name__ == '__main__':
     wpilib.run(PhotonVisionTest)

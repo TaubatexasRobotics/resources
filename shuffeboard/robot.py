b2a2ecc3a23ff4afb26dfc652e44df17844188f8
Flávio Lima <flavi@Flavio-note-W11>
2023-03-11 22:13:46 -0300

    shuffleboard

---
+++ shuffeboard/robot.py
@@ -0,0 +1,20 @@
+import wpilib
+import wpilib.shuffleboard
+
+Shuffleboard = wpilib.shuffleboard.Shuffleboard
+
+chooser = wpilib.SendableChooser()
+chooser.addOption('option1', '1')
+chooser.addOption('option2', '2')
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):        
+        self.sd = Shuffleboard.getTab("My Tab")
+        wpilib.SmartDashboard.putData('Choice', chooser)
+    
+    def robotPeriodic(self):
+        value = chooser.getSelected()
+        print(value)
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)

16c794147248c22d4a90b1d1af695a3955876e76
Flávio Lima <flavi@Flavio-note-W11>
2023-03-14 16:13:21 -0300

    (digital input) feat: receives digital input

---
+++ digitalnput/robot.py
@@ -0,0 +1,15 @@
+import wpilib
+
+limitSwitch = wpilib.DigitalInput(0);
+analog_input = wpilib.AnalogInput(0)
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+        pass
+    def robotPeriodic(self):
+        # print(analog_input.getVoltage())
+        print(limitSwitch.get())
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
